0eb64a2aa1f8bb256649ec41f89829df6bfecc9a
Jose Herrera <josevictorherrera@gmail.com>
2018-10-16 07:08:19 -0700

    combine google fonts link tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,9 +15,7 @@ module.exports = {
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Merriweather:400,700'},
-      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Oswald:400,700'},
-      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Inconsolata'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Merriweather:400,700|Oswald:400,700|Inconsolata:400'},
       {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/6.0.0/sanitize.min.css'}
     ]
   },
